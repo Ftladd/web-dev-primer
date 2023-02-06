f49f5f303dbefdf7c377abce4cd7ba1556e3086d
Ftladd <finlay.ladd@gmail.com>
2023-02-05 22:06:00 -0600

    student types added

---
+++ src/types/studentTypes.d.ts
@@ -0,0 +1,31 @@
+type CourseGrades = {
+  assignmentWeights: Array<CourseGrade>;
+  finalExamWeight: number;
+};
+type CourseGrade = {
+  name: string;
+  weight: number;
+  grade: number;
+};
+type Student = {
+  name: string;
+  weights: CourseGrades;
+  currentAverage: number;
+};
+type NewStudentRequest = {
+  name: string;
+  weights: CourseGrades;
+};
+type AssignmentGrade = {
+  grade: number;
+};
+type FinalGrade = {
+  overallScore: number;
+  letterGrade: string;
+};
+type FinalExamScores = {
+  neededForA: number;
+  neededForB: number;
+  neededForC: number;
+  neededForD: number;
+};
